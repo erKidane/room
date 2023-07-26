45021f5c847292743ce16083acd1f4349ffbe206
admes <kidane.ermiyas@yahoo.de>
2023-07-26 23:52:04 +0200

    update2

--- app/src/main/java/com/example/abschlussaufgabe/ui/HomeFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/HomeFragment.kt
@@ -5,8 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.abschlussaufgabe.R
+import com.example.abschlussaufgabe.databinding.FragmentHomeBinding
+import com.example.abschlussaufgabe.viewmodel.ViewModel
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private val viewModel: ViewModel by activityViewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStart.setOnClickListener{
+            findNavController().navigate()
+        }
+
+    }
+
+
 
 }
